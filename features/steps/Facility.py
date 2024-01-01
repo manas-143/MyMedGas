75872bbe3a499950d7841c3524b826c903fb636e
Manas <122993008+manas-143@users.noreply.github.com>
2024-01-01 15:22:27 +0530

    Update Facility.py

--- features/steps/Facility.py
+++ features/steps/Facility.py
@@ -77,7 +77,7 @@ def adding_a_new_facility(context):
         send_input(context.page, locators["FACILITY STD SELECT"], rows["Standard"])
         context.page.keyboard.press("Enter")
         dt = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        send_input(context.page, locators["FACILITY ADDRESS"], rows["Address"].format(dt))
+        send_input(context.page, locators["FACILITY ADDRESS"], rows["Address"])
         expect(context.page.locator(locators["LOGO"])).not_to_be_visible(timeout=50000)
         send_input(context.page, locators["FACILITY CITY"], rows["City"])
         if 'State' in rows and rows['State']:
